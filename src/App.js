3b834ce7c6f55126e1ea66cfa0e5a770cc893fe4
Arely Correa-Perez <arely.corre@hotmail.com>
2023-08-29 12:38:24 -0400

    Created paths to Services,Products, and Sign Up

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import { BrowserRouter  as Router,Routes, Route} from 'react-router-dom';
 import Home from './components/pages/Home';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
 
 function App() {
   return (
@@ -11,7 +14,11 @@ function App() {
       <Navbar />
       {/* Switch (statements) has been replaced with Routes */}
       <Routes>
+        {/* All these paths have images due to App.css on */}
        <Route exact path ='/' element = {<Home/>} /> 
+       <Route exact path='/services' Component={Services}/>
+       <Route exact path='/products' Component={Products}/>
+       <Route exact path='/sign-up' Component={SignUp}/>
       </Routes>
       </Router>
     </>
